ab6658af74bd2d73228f2af228611fa51c014bba
Gurveen <gurveensingh45@gmail.com>
2022-03-27 15:16:45 +0530

    Google Auth Completed

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,5 +1,5 @@
 import { combineReducers } from "redux";
-import authReducer from "./authReducer";
+import authReducer from "./authReducers";
 
 export default combineReducers({
   auth: authReducer,
